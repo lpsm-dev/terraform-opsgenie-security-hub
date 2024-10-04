c97214b9aba99baec581ad9746bba0c5379327cc
lpsm-dev-github <lpsm-dev@protonmail.com>
2024-10-04 03:07:39 -0300

    fix: setup content

--- opsgenie_integration.tf
+++ opsgenie_integration.tf
@@ -1,21 +1,4 @@
 # ==================================================================
-# CREATE OPSGENIE TEAM
-# ==================================================================
-
-resource "opsgenie_team" "this" {
-  name                     = var.opsgenie_team_name
-  description              = var.opsgenie_team_description
-  delete_default_resources = true
-  dynamic "member" {
-    for_each = try(data.opsgenie_user.users, [])
-    content {
-      id   = member.value.id
-      role = var.opsgenie_users[index(var.opsgenie_users.*.user_email, member.key)].role
-    }
-  }
-}
-
-# ==================================================================
 # CREATE OPSGENIE INTEGRATION
 # ==================================================================
 
