6dfb3ecdcaf16a54b573523a0de890a6673a5bf3
lpsm-dev-github <lpsm-dev@protonmail.com>
2024-10-05 12:54:29 -0300

    fix: default values

--- src/_variables.tf
+++ src/_variables.tf
@@ -5,6 +5,7 @@
 variable "aws_environment" {
   description = "The environment to deploy the resources"
   type        = string
+  default     = "production"
   validation {
     condition     = contains(["develop", "stage", "production", "sandbox", "pocs"], var.aws_environment) #
     error_message = "Invalid argument \"aws_environment\", please choose one of: (\"develop\",\"stage\",\"production\",\"sandbox\",\"pocs\")."
@@ -14,6 +15,7 @@ variable "aws_environment" {
 variable "aws_region" {
   description = "AWS region to deploy the resources"
   type        = string
+  default     = "us-east-1"
   validation {
     condition     = contains(["us-east-1", "sa-east-1"], var.aws_region) #
     error_message = "Invalid argument \"aws_region\", please choose one of: (\"us-east-1\",\"sa-east-1\")."
