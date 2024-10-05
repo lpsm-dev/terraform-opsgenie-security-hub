29b0334550feda993848b1971d3ee10a2dcb32ad
lpsm-dev-github <lpsm-dev@protonmail.com>
2024-10-04 22:06:58 -0300

    fix: small changes

--- aws_iam.tf
+++ aws_iam.tf
@@ -35,7 +35,6 @@ resource "aws_iam_policy" "this" {
     Version = "2012-10-17"
     Statement = [
       {
-        Sid      = "VisualEditor0"
         Effect   = "Allow"
         Action   = ["securityhub:GetFindings", "securityhub:BatchUpdateFindings"]
         Resource = ["arn:aws:securityhub:*:*:*"]
