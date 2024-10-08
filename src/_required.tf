0761203fa534ef5a9048b99f1135b83773b0e707
Lucca <lpsm-dev@protonmail.com>
2024-10-08 07:05:01 -0700

    fix: required providers

--- src/_required.tf
+++ src/_required.tf
@@ -5,9 +5,17 @@
 terraform {
   required_version = ">= 1.8"
   required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.70"
+    }
     opsgenie = {
       source  = "opsgenie/opsgenie"
       version = "~> 0.6"
+    }
+    time = {
+      source  = "hashicorp/time"
+      version = "~> 0.12"
     }
   }
 }
